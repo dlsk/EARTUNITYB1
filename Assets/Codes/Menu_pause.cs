b9767932e78045fe95b3438894e08b4fc44450ff
MSI\Enzo Onesti <enzo.onesti@gmail.com>
2018-03-16 16:16:10 +0100

    Modif

--- Assets/Codes/Menu_pause.cs
+++ Assets/Codes/Menu_pause.cs
@@ -1,40 +1,75 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu_pause : MonoBehaviour
 {
     public GameObject panelPause;
     private bool isActive = false;
-
+    private bool index = false;
+    public string MenuScene;
 
     void Update()
     {
-        //menu affiché
-        if (isActive == true)
-        {
-            panelPause.SetActive(true);
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.Confined;
-            Time.timeScale = 0;
-        }
-
-        else
-        {
-            panelPause.SetActive(false);
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-            Time.timeScale = 1;
-        }
-
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             ResumeButton();
         }
+
+        //menu affiché
+        if (isActive == true)
+        {
+            panelPause.SetActive(true);
+            Time.timeScale = 0;
+
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                ChoicesButton();
+            }
+
+            if (index == false)
+            {               
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    ResumeButton();
+                }
+            }
+
+            if (index == true)
+            {
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    OnclickMenu();
+                }
+            }
+        }
+        else
+        {
+            panelPause.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
-   
+
     public void ResumeButton()
     {
         isActive = !isActive;
+    }
+
+    public void OnclickMenu()
+    {
+        SceneManager.LoadScene(MenuScene);
+
+    }
+
+    private void ChoicesButton()
+    {
+        index = !index;
+
+        if (index == false)
+            Debug.Log("Resume");
+
+        if (index == true)
+            Debug.Log("Menu");
     }
 }
