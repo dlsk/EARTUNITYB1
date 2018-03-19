5ee0b995281ff87f00810025d5e853d452b4255b
DESKTOP-EL3I4AT\Gaspard <gaspard.broussaud1999@gmail.com>
2018-03-19 15:09:09 +0100

    Fantome

--- Assets/Codes/TriggerTimeFantome.cs
+++ Assets/Codes/TriggerTimeFantome.cs
@@ -1,10 +1,43 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.SceneManagement;
 
 public class TriggerTimeFantome : MonoBehaviour {
 
-    public GameObject PlayerCamera;
+    public float triggerTimer = 0.0f;
+    public string GameScene;
+    public GameObject destroyobj1;
+    public GameObject destroyobj2;
     private float _registerFantomeTime;
+    private float _registered;
+    private Vector3 fantomePosition;
+    public GameObject PlayerCamera;
+    public GameObject Fantome;
+    public GameObject PlayerModel;
+    //private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        //_rigidbody = GetComponent<Rigidbody>();
+        //Assert.IsNotNull(_rigidbody);
+    }
+
+    private void Update()
+    {
+        triggerTimer -= Time.deltaTime;
+
+        if (_registerFantomeTime >= triggerTimer)
+        {
+            Apparition();
+        }
+
+        if (triggerTimer <= 0)
+        {
+            SceneManager.LoadScene("scene_lvl1");
+            Destroy(destroyobj1, 0.0f);
+            Destroy(destroyobj2, 0.0f);
+        }
+    }
 
     public void OnTriggerEnter(Collider other)
     {
@@ -12,7 +45,13 @@ public class TriggerTimeFantome : MonoBehaviour {
         {
             //Debug.Log("Ok c'est bon");
             _registerFantomeTime = PlayerCamera.GetComponent<Timer>().targetTime;
+            fantomePosition = PlayerModel.transform.position;
             Debug.Log(_registerFantomeTime);
         }
+    }
+
+    public void Apparition()
+    {
+        Fantome.transform.position = fantomePosition;
     }
 }
