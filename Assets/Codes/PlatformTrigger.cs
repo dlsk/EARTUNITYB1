14eee92f24a1d13ed688d1217be159c097540171
DESKTOP-EL3I4AT\Gaspard <gaspard.broussaud1999@gmail.com>
2018-03-21 13:33:38 +0100

    Fix Lvl2

---
+++ Assets/Codes/PlatformTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour {
+
+    public GameObject MurInvisible;
+    public GameObject PlayerModel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MurInvisible.transform.position = PlayerModel.transform.position;
+        }
+    }
+}
