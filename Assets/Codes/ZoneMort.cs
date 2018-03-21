b1909c0af3fd3bcf0d1aa8c46f2dcf059b81d2e3
MSI\Enzo Onesti <enzo.onesti@gmail.com>
2018-03-21 17:41:54 +0100

    zone de mort

---
+++ Assets/Codes/ZoneMort.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneMort : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject obj1;
+    public GameObject obj2;
+
+    void OnTriggerEnter(Collider player)
+    {
+        Application.LoadLevel(Application.loadedLevel);
+        Destroy(obj1, 0.0f);
+        Destroy(obj2, 0.0f);
+    }
+}
