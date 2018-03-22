5aa7ed03a0c99252ec06ed50ff218268232af58c
MSI\Enzo Onesti <enzo.onesti@gmail.com>
2018-03-22 17:23:46 +0100

    Trigger zone lvl 3, code boutons et animations portes, timer + fantôme

---
+++ Assets/Codes/CodeBoutonAnimLvL3.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeBoutonAnimLvL3 : MonoBehaviour
+{
+
+    public GameObject PlayerCamera;
+    public GameObject PlayerModel;
+    public GameObject TriggerFantome;
+    public GameObject BoutonAnim;
+    private float _registerFantomeTimeBouton;
+    private float currentTime;
+    private bool testTriggerBouton;
+    private bool onOrOff = true;
+    public bool animationOn = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        testTriggerBouton = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_registerFantomeTimeBouton >= TriggerFantome.GetComponent<TriggerTimeFantome>().triggerTimer && onOrOff == true)
+        {
+            onOrOff = !onOrOff;
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.E) && testTriggerBouton == true)
+        {
+            Debug.Log("Appuis");
+            _registerFantomeTimeBouton = PlayerCamera.GetComponent<Timer>().targetTime;
+            BoutonAnim.GetComponent<AnimationBouton>().AnimationBoutonPlay();
+            testTriggerBouton = !testTriggerBouton;
+        }
+    }
+
+}
---
+++ Assets/Codes/ScriptAnimPorte1.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptAnimPorte1 : MonoBehaviour
+{
+
+    public float timeAnimationAtLaunch = 0.0f;
+    public float currentTime = 0.0f;
+    private bool soundOn = false;
+    private bool timerOn = true;
+    public GameObject animPorte1;
+    public GameObject animBouton;
+    public Animator animationPorte1;
+    public GameObject BoutonBoolen;
+
+    private void Awake()
+    {
+        animationPorte1 = animPorte1.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animationPorte1.speed = 0.0f;
+    }
+
+    void Update()
+    {
+
+        if (BoutonBoolen.GetComponent<AnimationBouton>().animationPassée == true)
+        {
+            if (timerOn == true)
+            {
+                currentTime += Time.deltaTime;
+            }
+
+            if (currentTime >= 0.7f)
+            {
+                animationPorte1.speed = 1.0f;
+                animBouton.GetComponent<AnimationBouton>().animationBouton.speed = 0.0f;
+            }
+
+            /* if (currentTime >= 0.7f)
+             {
+                 VoiceActivation.Instance.MakeSonPont();
+                 soundOn = !soundOn;
+                 timerOn = !timerOn;
+                 currentTime = timeAnimationAtLaunch;
+             }
+         }
+
+         if (soundOn == true)
+         {
+             VoiceActivation.Instance.MakeSonPont();
+             //Debug.Log("BUUUUUG");
+             soundOn = !soundOn;
+         } */
+        }
+    }
+}
