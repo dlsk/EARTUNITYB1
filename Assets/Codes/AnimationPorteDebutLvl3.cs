d1b3e70fb4f0485613df2c4704acce199dd18a0f
DESKTOP-EL3I4AT\Gaspard <gaspard.broussaud1999@gmail.com>
2018-03-22 18:32:10 +0100

    Lvl 3 portes

---
+++ Assets/Codes/AnimationPorteDebutLvl3.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationPorteDebutLvl3 : MonoBehaviour
+{
+
+    public GameObject Porte;
+    public Animator animationPorte;
+    public static AnimationBouton Instance;
+
+
+    void Awake()
+    {
+        animationPorte = Porte.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        animationPorte.speed = 1.0f;
+        //AnimationPortePlay();
+    }
+
+    /*public void AnimationPortePlay()
+    {
+        animationPorte.speed = 1.0f;
+    }*/
+}
---
+++ Assets/Codes/TriggerFantomeLvl3.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+
+public class TriggerFantomeLvl3 : MonoBehaviour
+{
+
+    public float triggerTimer;
+    private bool onOrOff = true;
+    private bool testPositionTaken = true;
+    public bool Restart = false;
+    public string GameScene;
+    public GameObject destroyPlayer;
+    public GameObject destroyController;
+    public GameObject Porte;
+    private float _registerFantomeTime;
+    private float _registered;
+    private Vector3 fantomePosition;
+    public GameObject PlayerCamera;
+    public GameObject Fantome;
+    public GameObject PlayerModel;
+
+    private void Start()
+    {
+        triggerTimer = 3 * PlayerCamera.GetComponent<Timer>().timeInScene;
+        //Debug.Log(triggerTimer);
+    }
+
+    private void Update()
+    {
+        triggerTimer -= Time.deltaTime;
+
+        if (_registerFantomeTime >= triggerTimer && onOrOff == true)
+        {
+            Apparition();
+            onOrOff = !onOrOff;
+        }
+
+        if (triggerTimer <= 0)
+        {
+            Restart = !Restart;
+            SceneManager.LoadScene(GameScene);
+            Destroy(destroyPlayer, 0.0f);
+            Destroy(destroyController, 0.0f);
+        }
+
+        if (triggerTimer <= 120.0f)
+        {
+            //Porte.GetComponent<AnimationPorteDebutLvl3>().AnimationPortePlay();
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && testPositionTaken == true)
+        {
+            //Debug.Log("Ok c'est bon");
+            _registerFantomeTime = PlayerCamera.GetComponent<Timer>().targetTime;
+            fantomePosition = PlayerModel.transform.position;
+            testPositionTaken = !testPositionTaken;
+            //Debug.Log(_registerFantomeTime);
+        }
+    }
+
+    public void Apparition()
+    {
+        Fantome.transform.position = fantomePosition;
+    }
+}
